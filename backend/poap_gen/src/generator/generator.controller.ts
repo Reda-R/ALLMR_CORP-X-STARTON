67915c777943690da2d47901630abb344aac0c4b
Marc Zhou <marc.zhou@epitech.eu>
2022-05-08 18:11:31 +0200

    ADD all

--- backend/poap_gen/src/generator/generator.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Injectable, Post } from '@nestjs/common';
-
-@Injectable()
-export class GeneratorController {
-    @Post()
-
-}
--- poap_gen/src/components/UserEvent.vue
+++ poap_gen/src/components/UserEvent.vue
@@ -1,41 +1,55 @@
 <template>
-    <v-container fluid >
-        <br/>
-        <div class="col-12 secondary d-inline-flex">
-            <h1 class="col-3 white--text"> {{event.name}}</h1>
-            <div class="col-1"></div>
-            <v-img class="col-3" max-height="200" max-width="300" :src="event.url"></v-img>
-            <div class="col-1"/>
-            <v-col>
-                <v-btn :disabled="isActive" class="pa-12" @click="sendPoap">Send</v-btn>
-            </v-col>
-        </div>
-        <br/>
-    </v-container>
+  <v-container fluid>
+    <br />
+    <div class="col-12 secondary d-inline-flex">
+      <h1 class="col-3 white--text">{{ event.name }}</h1>
+      <div class="col-1"></div>
+      <v-img
+        class="col-3"
+        max-height="200"
+        max-width="300"
+        :src="event.url"
+      ></v-img>
+      <div class="col-1" />
+      <v-col>
+        <v-btn :disabled="isActive" class="pa-12" @click="sendPoap">Send</v-btn>
+      </v-col>
+    </div>
+    <br />
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  name: 'UserEvent',
+  name: "UserEvent",
   props: {
     event: {
-        type: Object
-    }
-  }, data: () => {
-      return {
-      isActive: false
-      }
-  }, methods : {
-      sendPoap() {
-          console.log("sendPoap", "clicked")
-        axios.post('http://localhost:8080/', this.event)
-            .then(() => {
-                console.log("sendPoap", "response")
-                this.isActive = true
-            })
-            .catch(() => console.log("sendPoap", "error"))
-      }
-  }
-}
+      type: Object,
+    },
+  },
+  data: () => {
+    return {
+      isActive: false,
+    };
+  },
+  methods: {
+    sendPoap() {
+      console.log("sendPoap", "clicked");
+      axios
+        .post("http://localhost:3000/generate", {
+          img: this.event.image,
+          image_name: this.event.name,
+          name: this.event.name,
+          description: "description",
+          to: "0x614ACFfC68508d8F0C01Bd235Ee8ae622F8E2558",
+        })
+        .then(() => {
+          console.log("sendPoap", "response");
+          this.isActive = true;
+        })
+        .catch(() => console.log("sendPoap", "error"));
+    },
+  },
+};
 </script>
